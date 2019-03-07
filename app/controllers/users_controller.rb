9d4a08c2c8bc7905059b9b617989b31d5ed0ed67
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2019-03-07 20:37:40 +0900

    add: sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録ありがとうございます"
+      redirect_to @user # redirect_to user_url(@user)と等価
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :status, :password, :password_confirmation)
+    end
 end
