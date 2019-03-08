54881b6df007ddd8f52ac1d7ec18b432c5f4b7d6
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2019-03-08 18:18:11 +0900

    add: friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,10 +14,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "登録ありがとうございます"
+      flash[:success] = "ご登録ありがとうございます"
       redirect_to @user # redirect_to user_url(@user)と等価
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールが更新されました"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -22,5 +37,22 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :status, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
