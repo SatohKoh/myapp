812f03fb221298d53e125696329c335702d38513
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2019-03-09 17:17:22 +0900

    add: index and destroy of User model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # テストユーザーがログイン中の場合にtrueを返す
@@ -18,7 +17,6 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-
   # テストユーザーとしてログインする
   def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
